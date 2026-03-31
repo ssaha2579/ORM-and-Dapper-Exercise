ba15718cb510eabc49aed3a8a06ef4ed9c6049d6
Sagar Saha <Sagarsaha2579@gmail.com>
2026-03-31 17:43:37 -0400

    PascalCaseing and added product 149

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -13,35 +13,35 @@ string connString = config.GetConnectionString("DefaultConnection");
 IDbConnection conn = new MySqlConnection(connString);
 
 #region Department Selection
-//var departmentRepo = new DapperDepartmentRepository(conn);
+var departmentRepo = new DapperDepartmentRepository(conn);
 
-//departmentRepo.InsertDepartment("Sagar's New Department");
+departmentRepo.InsertDepartment("Sagar's New Department");
 
-//var departments = departmentRepo.GetAllDepartments();
+var departments = departmentRepo.GetAllDepartments();
 
-//foreach (var department in departments)
-//{
-//    Console.WriteLine(department.Name);
-//    Console.WriteLine(department.DepartmentID);
-//    Console.WriteLine();
-//    Console.WriteLine(); 
+foreach (var department in departments)
+{
+    Console.WriteLine(department.Name);
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine();
+    Console.WriteLine();
 
-//}
+}
 #endregion
 
 var productRepository = new DapperProductRepository(conn);
 
-//var productToUpdate = productRepository.GetProduct(1);
+var producttoupdate = productRepository.GetProduct(149);
 
-//productToUpdate.Name = "UPDATED!"; 
-//productToUpdate.Price = 12.99; 
-//productToUpdate.StockLevel = 1;
-//productToUpdate.OnSale = false;
+producttoupdate.Name = "updated!";
+producttoupdate.Price = 12.99;
+producttoupdate.StockLevel = 1;
+producttoupdate.OnSale = false;
 
 
-//productRepository.Update(productToUpdate);
+productRepository.Update(producttoupdate);
 
-productRepository.Delete(1); 
+productRepository.Delete(149); 
 
 var products = productRepository.GetAllProducts();
 foreach (var product in products)
